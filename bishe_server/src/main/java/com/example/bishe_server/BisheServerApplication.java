5638616b89d5e81d7a2862d4081a9d096b15b815
qiushibo <1114676346@qq.com>
2023-03-22 21:47:18 +0800

    server端初始化

---
+++ bishe_server/src/main/java/com/example/bishe_server/BisheServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bishe_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisheServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BisheServerApplication.class, args);
+    }
+
+}
---
+++ bishe_server/src/test/java/com/example/bishe_server/BisheServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bishe_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisheServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
