79cb67e0c2734b845b8826b527b21f1db83a9032
qiushibo <1114676346@qq.com>
2023-05-03 00:57:52 +0800

    后端提交，mqtt

--- src/main/java/com/example/server_parking/controller/RailingController.java
+++ src/main/java/com/example/server_parking/controller/RailingController.java
@@ -1,13 +1,13 @@
 package com.example.server_parking.controller;
 
 
+import com.example.server_parking.dao.ParkingInfoDao;
 import com.example.server_parking.mqtt.MQTTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -22,4 +22,19 @@ public class RailingController {
         mqttService.sendLiftGateCommand();
         return ResponseEntity.ok().build();
     }
+
+    @Autowired
+    private ParkingInfoDao parkingInfoDao;
+
+    @PostMapping("/distance")
+    public ResponseEntity<Void> handleDistance(@RequestBody Map<String, String> params) {
+        String distanceStr = params.get("distance");
+        double distance = Double.parseDouble(distanceStr);
+
+        int parking_is_useful = distance < 2 ? 1 : 0;
+        mqttService.setParkingIsUseful(parking_is_useful);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/example/server_parking/mqtt/MQTTService.java
+++ src/main/java/com/example/server_parking/mqtt/MQTTService.java
@@ -1,11 +1,11 @@
 package com.example.server_parking.mqtt;
 import com.example.server_parking.controller.CarInfoController;
-import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import com.example.server_parking.dao.ParkingInfoDao;
+import com.example.server_parking.entity.ParkingInfoEntity;
+import org.eclipse.paho.client.mqttv3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +13,10 @@ public class MQTTService {
     private static final String MQTT_BROKER_URI = "tcp://175.24.202.87:1883";
     private static final String MQTT_CLIENT_ID = "parking_server";
     private static final String MQTT_TOPIC = "parking/lift_gate";
-
+    private static final String MQTT_TOPIC_DISTANCE = "railing/distance";
     private IMqttClient mqttClient;
-
+    @Autowired
+    private ParkingInfoDao parkingInfoDao;
     public MQTTService() {
         try {
             mqttClient = new MqttClient(MQTT_BROKER_URI, MQTT_CLIENT_ID);
@@ -25,6 +26,38 @@ public class MQTTService {
             logger.error("没连上");
             e.printStackTrace();
         }
+        try {
+            mqttClient = new MqttClient(MQTT_BROKER_URI, MQTT_CLIENT_ID);
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    // handle connection lost
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    if (MQTT_TOPIC_DISTANCE.equals(topic)) {
+                        String distanceStr = new String(message.getPayload());
+                        double distance = Double.parseDouble(distanceStr);
+                        int parking_is_useful = distance < 200 ? 1 : 0;
+                        setParkingIsUseful(parking_is_useful);
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    // handle delivery complete
+                }
+            });
+
+            mqttClient.connect();
+            mqttClient.subscribe(MQTT_TOPIC_DISTANCE);
+        } catch (MqttException e) {
+            Logger logger = LoggerFactory.getLogger(CarInfoController.class);
+            logger.error("没连上");
+            e.printStackTrace();
+        }
+
     }
 
     public void sendLiftGateCommand() {
@@ -34,5 +67,12 @@ public class MQTTService {
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
+    public void setParkingIsUseful(int parking_is_useful) {
+        String parking_id = "2c25f2ad-e82d-4a20-acc3-161054a417f5"; // Replace with your actual parking_id
+        ParkingInfoEntity parkingInfoEntity = new ParkingInfoEntity();
+        parkingInfoEntity.setParking_id(parking_id);
+        parkingInfoEntity.setParking_is_useful(parking_is_useful);
+        parkingInfoDao.updateUseFul(parkingInfoEntity);
     }
 }
